208504e4a9f8fd136bc4f2fb569dc9f13a143878
ulrich hamaani <119246440+ulrichhamaani237@users.noreply.github.com>
2024-09-27 16:15:09 +0100

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'production' => false,
+    'baseUrl' => '',
+    'title' => 'Dashboard',
+    'description' => 'Dashboard template built with tailwindcss 🛩',
+    'collections' => [],
+    'name' => 'ulrich',
+];
